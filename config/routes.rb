e4211385f0a77dc2e3124677703cef09e81b962c
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-06-18 19:26:29 +0100

    add login form, new and create actions in sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   root to: 'main#index'
-  delete "logout", to: 'sessions#destroy'
+  
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  
+  delete "logout", to: 'sessions#destroy'
+
   get 'about-us', to: "about#index", as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
