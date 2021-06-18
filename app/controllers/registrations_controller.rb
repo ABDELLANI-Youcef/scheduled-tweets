f7940bf2f53f478cd145b802c931a4aedc4816a5
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-06-18 17:05:36 +0100

    Implement Registration create method and add error alert div in the Sign up page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,5 +4,19 @@ class RegistrationsController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    
+    params.require(:user).permit(:email, :password, :password_confirmation)
+    
   end
 end
