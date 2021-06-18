c3144b258a0c3c1a9727161a1e369938d1a4712d
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-06-18 14:20:38 +0100

    create Registration controller with new and create actions and add sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'registrations/new'
+  get 'registrations/create'
   root to: 'main#index'
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   get 'about-us', to: "about#index", as: :about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
