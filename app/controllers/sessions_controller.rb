27bb7da5a1fd9ec49ee1e614fc59e404bddda1ed
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-06-19 23:58:03 +0100

    fix linters errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged in successfully"
+      redirect_to root_path, notice: 'Logged in successfully'
     else
-      flash[:alert] = "Invalid email or password"
+      flash[:alert] = 'Invalid email or password'
       render :new
     end
   end
-  
 
   def destroy
     session[:user_id] = nil
--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -1,12 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class PasswordsControllerTest < ActionDispatch::IntegrationTest
-  test "should get edit" do
+  test 'should get edit' do
     get passwords_edit_url
     assert_response :success
   end
 
-  test "should get update" do
+  test 'should get update' do
     get passwords_update_url
     assert_response :success
   end
