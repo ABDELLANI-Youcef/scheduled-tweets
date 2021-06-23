1e341100fe4d42d02c982e1f928d7cd922c66756
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2021-06-23 01:00:28 +0100

    add mail body in the reset.html.erb and reset.text.erb, send mail from Password mailer, change deliver_later to deliver_now in PasswordResetsController create method ajd correct placeholder in email field taf in the reset password view

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,7 +5,7 @@ class PasswordResetsController < ApplicationController
     @user = User.find_by(email: params[:email])
 
     if @user.present?
-      PasswordMailer.with(user: @user).reset.deliver_later
+      PasswordMailer.with(user: @user).reset.deliver_now
     end
     redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password."
   end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   
   get 'password/reset', to: "password_resets#new"
   post 'password/reset', to: "password_resets#create"
+  get 'password/reset/edit', to: "password_resets#edit"
+  patch 'password/reset/edit', to: "password_resets#update"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
